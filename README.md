f32547d89d73d8a76191e7ecb246aac7a2336d83
benjovitz <dbenjovitz@gmail.com>
2023-03-23 13:03:19 +0100

    first

---
+++ README.md
@@ -0,0 +1,22 @@
+# 3sem1projekt_backend
+# Gruppekontrakt
+
+## Medlemmer: 
+<p>Daniel Benjovitz, dani37i9@stud.kea.dk</p>
+<p>Lasse Dall Mikkelsen, lass794r@stud.kea.dk</p>
+<p>Oscar Storm, osca4711@stud.kea.dk</p>
+<p>Jacob Anker-Møller, jaco718v@stud.kea.dk</p>
+
+## Mødeplan: 
+Vi planlægger at mødes fysisk hver mandag og online de resterende dage.
+### Mandag:
+Det fysiske møde er til for at planlægge og aftale, det kan være svært at forklarer sine ideer online og blive enige om det, så der skal som sådan ikke kodes om mandagen. Mandag er til for at afstemme opgaver.
+### De resterende dage:
+Vi vil have et scrum møde online om morgen (vi aftaler nærmere tidspunkt mandag) til at afstemme dagens opgaver, og derefter et møde senere på dagen, hvor vi snakker om evt. udfordringer eller successoplevelser.
+## Mødedisciplin:
+Vi er fleksible i forhold til mødetid kan ændres, men når det er aftalt er det aftalt.
+Mødes der ikke op en dag forventes der en forklaring til hvorfor.
+Mødes der slet ikke op i løbet af projektet, hverken fysisk eller online, vil dette blive noteret til aflevering at personen manglet under hele eller flere dage af projektet.
+## Branching strategi:
+Vi forventer at tage den fysisk mandag.
+Daniel lægger Azure konto til.
---
+++ index.js
@@ -0,0 +1,64 @@
+//import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
+import "./navigo_EditedByLars.js"  //Will create the global Navigo, with a few changes, object used below
+//import "./navigo.min.js"  //Will create the global Navigo object used below
+
+import {
+  setActiveLink, adjustForMissingHash, renderTemplate, loadTemplate
+} from "./utils.js"
+
+import { initLogin,logout } from "./pages/login/login.js"
+import { initSignup } from "./pages/signup/signup.js"
+
+
+window.addEventListener("load", async () => {
+
+
+  const templateSignup = await loadTemplate("./pages/signup/signup.html")
+  const templateLogin = await loadTemplate("./pages/login/login.html")
+  const templateNotFound = await loadTemplate("./pages/notFound/notFound.html")
+
+  adjustForMissingHash()
+
+  const router = new Navigo("/", { hash: true });
+  //Not especially nice, BUT MEANT to simplify things. Make the router global so it can be accessed from all js-files
+  window.router = router
+
+  router
+    .hooks({
+      before(done, match) {
+        setActiveLink("menu", match.url)
+        done()
+      }
+    })
+    .on({
+      //For very simple "templates", you can just insert your HTML directly like below
+      "/": () => document.getElementById("content").innerHTML = `
+        <h2>Home</h2>
+        <img style="width:50%;max-width:600px;margin-top:1em;" src="./images/cars.png">
+        <p style='margin-top:1em;font-size: 1.5em;color:darkgray;'>
+          Car's 'R' Us - Created, as a help to make GREAT fullstack developers <span style='font-size:2em;'>&#128516;</span>
+        </p>
+     `,
+      "/signup": () => {
+        renderTemplate(templateSignup, "content")
+        initSignup()
+      },
+      "/login": () => {
+        renderTemplate(templateLogin, "content")
+        initLogin()
+      },
+      "/logout": () => {
+        logout()
+      }
+    })
+    .notFound(() => {
+      renderTemplate(templateNotFound, "content")
+    })
+    .resolve()
+});
+
+
+window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+  alert('Error: ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber
+    + ' Column: ' + column + ' StackTrace: ' + errorObj);
+}
