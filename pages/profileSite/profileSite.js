d79fe9185b9777da6f3a0b8031bc70a08bb5978a
benjovitz <dbenjovitz@gmail.com>
2023-03-31 13:57:38 +0200

    update user good

--- pages/profileSite/profileSite.js
+++ pages/profileSite/profileSite.js
@@ -2,19 +2,11 @@ import { handleHttpErrors } from "../../utils.js";
 import { getHeaders,API_URL } from "../../settings.js";
 
 let headers = getHeaders()
+headers.append("content-type","application/json")
 export async function initUserProfile(){
+document.getElementById("btn-save-userinfo").onclick=updateUserInfo
+getUserInfo()
 
-    try {
-        const response = await fetch(API_URL+"/users/userprofile",{
-            headers: headers
-        }).then(handleHttpErrors)
-        renderUserInfo(response)
-       document.getElementById("user-rating").innerText=response.ranking
-       const reviews = `` 
-        console.log(response)
-    } catch (error) {
-        
-    }
 }
 
 function renderUserInfo(response){
@@ -33,8 +25,44 @@ function renderUserInfo(response){
     <li><input id="city" type="text" value="${response.city}"></li>
     <label for="zip">Zip</label>
     <li><input id="zip" type="text" value="${response.zip}"></li>
-    <button id="btn-save-userinfo">Save changes</button>
+    <label for="password">Password</label>
+    <li><input id="password" type="password"></li>
     </ul>
     `
     document.getElementById("user-information").innerHTML=userInfo
+}
+
+async function updateUserInfo(){
+    let newUserInfo={}
+    newUserInfo.fullName = document.getElementById("fullName").value
+    newUserInfo.email = document.getElementById("email").value
+    newUserInfo.phone = document.getElementById("phone").value
+    newUserInfo.address = document.getElementById("address").value
+    newUserInfo.city = document.getElementById("city").value
+    newUserInfo.zip = document.getElementById("zip").value
+    newUserInfo.password=document.getElementById("password").value
+
+    try {
+        await fetch(API_URL+"/users/"+localStorage.getItem("user"),{
+            method: "put",
+            headers:headers,
+            body:JSON.stringify(newUserInfo)
+        }).then(handleHttpErrors)
+        document.getElementById("success").innerText="New info saved"
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getUserInfo(){
+    try {
+        const response = await fetch(API_URL+"/users/userprofile",{
+            headers: headers
+        }).then(handleHttpErrors)
+        renderUserInfo(response)
+       document.getElementById("user-rating").innerText=response.ranking
+        console.log(response)
+    } catch (error) {
+        
+    }
 }
